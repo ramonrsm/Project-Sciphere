fe6373b9fe1c7ae3967b59b1dd5c3d1235e8f445
ramonrsm <8749Rsm2716git>
2018-02-19 20:55:15 -0300

    Implementando nova câmera 0.1

---
+++ Assets/Scripts/Cameras/Nova Mecanica/FolowPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolowPlayer : MonoBehaviour {
+
+	public GameObject	target;	
+	public float distanceYTarget = 1.2f;
+	public float distanceZTarget = 4f;
+
+	public float distancia;
+
+	// Use this for initialization
+	void Start () {
+		viewCamera01();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void LateUpdate()
+	{
+		verificarAlturaTarget();
+		transform.position = new Vector3(target.transform.position.x, distanceYTarget, target.transform.position.z - distanceZTarget);
+	}
+
+	void verificarAlturaTarget(){
+
+		float alturaTarget = target.transform.position.y;
+		float alturCamera = transform.position.y + distanceYTarget;
+
+		distancia = alturCamera - alturaTarget;
+
+		
+	}
+
+	void viewCamera01(){
+
+		transform.position = new Vector3(target.transform.position.x, distanceYTarget + distanceYTarget, distanceZTarget);
+		transform.rotation = Quaternion.Euler(7f, 0, 0);
+	}
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -21,7 +21,6 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-		
 	}
 	
 	// Update is called once per frame
