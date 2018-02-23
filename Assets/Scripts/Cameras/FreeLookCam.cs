20552df93123167c3f917a4f5bf71695b9f7a099
ramonrsm <8749Rsm2716git>
2018-02-23 15:08:47 -0300

    Melhorias câmera mobile

--- Assets/Scripts/Cameras/FreeLookCam.cs
+++ Assets/Scripts/Cameras/FreeLookCam.cs
@@ -15,6 +15,7 @@ namespace UnityStandardAssets.Cameras
 
         [SerializeField] private float m_MoveSpeed = 1f;                      // How fast the rig will move to keep up with the target's position.
         [Range(0f, 10f)] [SerializeField] private float m_TurnSpeed = 1.5f;   // How fast the rig will rotate from user input.
+        [Range(0f, 10f)] [SerializeField] private float m_TurnSpeedMoblie = 1.5f;
         [SerializeField] private float m_TurnSmoothing = 0.0f;                // How much smoothing to apply to the turn input, to reduce mouse-turn jerkiness
         [SerializeField] private float m_TiltMax = 75f;                       // The maximum value of the x axis rotation of the pivot.
         [SerializeField] private float m_TiltMin = 45f;                       // The minimum value of the x axis rotation of the pivot.
@@ -74,12 +75,59 @@ namespace UnityStandardAssets.Cameras
             transform.position = Vector3.Lerp(transform.position, m_Target.position, deltaTime*m_MoveSpeed);
         }
 
-        private void HandleRotationMovementMobile(){
+        private void HandleRotationMovementMobile()
+        {
 
+            if(Input.touchCount == 1){
+
+                Touch touch = Input.GetTouch(0);
+
+                    if(touch.phase == TouchPhase.Moved){
+                    // Read the user input
+                    var x = touch.deltaPosition.x;
+                    var y = touch.deltaPosition.y;
+
+                    // Adjust the look angle by an amount proportional to the turn speed and horizontal input.
+                    m_LookAngle += x*m_TurnSpeedMoblie;
+
+                    // Rotate the rig (the root object) around Y axis only:
+                    m_TransformTargetRot = Quaternion.Euler(0f, m_LookAngle, 0f);
+
+                    if (m_VerticalAutoReturn)
+                    {
+                        // For tilt input, we need to behave differently depending on whether we're using mouse or touch input:
+                        // on mobile, vertical input is directly mapped to tilt value, so it springs back automatically when the look input is released
+                        // we have to test whether above or below zero because we want to auto-return to zero even if min and max are not symmetrical.
+                        m_TiltAngle = y > 0 ? Mathf.Lerp(0, -m_TiltMin, y) : Mathf.Lerp(0, m_TiltMax, -y);
+                    }
+                    else
+                    {
+                        // on platforms with a mouse, we adjust the current angle based on Y mouse input and turn speed
+                        m_TiltAngle -= y*m_TurnSpeedMoblie;
+                        // and make sure the new value is within the tilt range
+                        m_TiltAngle = Mathf.Clamp(m_TiltAngle, -m_TiltMin, m_TiltMax);
+                    }
+
+                    // Tilt input around X is applied to the pivot (the child of this object)
+                    m_PivotTargetRot = Quaternion.Euler(m_TiltAngle, m_PivotEulers.y , m_PivotEulers.z);
+
+                    if (m_TurnSmoothing > 0)
+                    {
+                        m_Pivot.localRotation = Quaternion.Slerp(m_Pivot.localRotation, m_PivotTargetRot, m_TurnSmoothing * Time.deltaTime);
+                        transform.localRotation = Quaternion.Slerp(transform.localRotation, m_TransformTargetRot, m_TurnSmoothing * Time.deltaTime);
+                    }
+                    else
+                    {
+                        m_Pivot.localRotation = m_PivotTargetRot;
+                        transform.localRotation = m_TransformTargetRot;
+                    }
+                }
+            }
         }
 
         private void HandleRotationMovementPC()
         {
+
 			if(Time.timeScale < float.Epsilon)
 			return;
 
@@ -123,7 +171,6 @@ namespace UnityStandardAssets.Cameras
 			}
         }
 
-        
         public void ativarControleMobile(){
 		    controleMobile = true;
 		    controlePC = !controleMobile;
--- Assets/Scripts/Player/PlayerMovimentacao.cs
+++ Assets/Scripts/Player/PlayerMovimentacao.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerMovimentacao : MonoBehaviour {
 
 	[SerializeField] private float	velocidadeMaxGiro = 25;
+	[SerializeField] private float	velocidadeMaxGiroMobile = 45;
 	[SerializeField] private float	forcaMovimento = 5;
+	[SerializeField] private float	forcaMovimentoMobile = 2;
 	private Rigidbody	playerRigidbody;
 	private Vector3	    mover;
 	private Transform 	posCamera;
 	private Vector3 	posCameraFrente;
 
-	private GameObject FreeLookCameraRig;
+	public GameObject FreeLookCameraRig;
 	public bool controleMobile, controlePC = true;
 
 	private float h, v;
@@ -20,35 +22,36 @@ public class PlayerMovimentacao : MonoBehaviour {
 		
 		if (Camera.main != null) {
             posCamera = Camera.main.transform;
+
+			FreeLookCameraRig = GameObject.Find("FreeLookCameraRig");
+
+			if(FreeLookCameraRig != null){
+				FreeLookCameraRig.transform.SendMessage("ativarControleMobile", SendMessageOptions.DontRequireReceiver);
+			}else{
+				Debug.Log("Aviso: FreeLookCameraRig não foi encontrado");
+				return;
+			}
         }
         else {
             Debug.LogWarning("Aviso: nenhuma câmera principal encontrada. É precisa de uma Câmera marcada como \"MainCamera \""+
 							 "para controles relativos à câmera");
 			return;
         }
-
-		FreeLookCameraRig = GameObject.Find("FreeLookCameraRig");
-
-		if(FreeLookCameraRig != null){
-			FreeLookCameraRig.transform.SendMessage("ativarControleMobile", SendMessageOptions.DontRequireReceiver);
-		}else{
-			Debug.Log("Aviso: FreeLookCameraRig não foi encontrado");
-			return;
-		}
 	}
 
 	// Use this for initialization
 	void Start () {
 		
+		playerRigidbody = GetComponent<Rigidbody>();
+
 		if(controleMobile){
-			transform.SendMessage("ativarControleMobile", true, SendMessageOptions.DontRequireReceiver);
+			playerRigidbody.maxAngularVelocity = velocidadeMaxGiroMobile;
+			FreeLookCameraRig.SendMessage("ativarControleMobile", SendMessageOptions.DontRequireReceiver);
 		}
 		else if(controlePC){
-			transform.SendMessage("ativarControlePC", true, SendMessageOptions.DontRequireReceiver);
+			playerRigidbody.maxAngularVelocity = velocidadeMaxGiro;
+			FreeLookCameraRig.SendMessage("ativarControlePC", SendMessageOptions.DontRequireReceiver);
 		}
-
-		playerRigidbody = GetComponent<Rigidbody>();
-		playerRigidbody.maxAngularVelocity = velocidadeMaxGiro;
 	}
 	
 	// Update is called once per frame
@@ -68,7 +71,7 @@ public class PlayerMovimentacao : MonoBehaviour {
 
             // calcular direção relativa a câmera:
             posCameraFrente = Vector3.Scale(posCamera.forward, new Vector3(1, 0, 1)).normalized;
-            mover = (v*posCameraFrente + h*posCamera.right).normalized;
+			mover = (v*posCameraFrente + h*posCamera.right).normalized;
         }
         else {
 			 // Usar direção relativa ao mundo em caso de não haver cÂmera principal
@@ -78,7 +81,12 @@ public class PlayerMovimentacao : MonoBehaviour {
 
 	void FixedUpdate() {
 		
-		playerRigidbody.AddForce(mover * forcaMovimento);
+		if(controleMobile){
+			playerRigidbody.AddForce(mover * forcaMovimentoMobile);
+		}
+		else if(controlePC){
+			playerRigidbody.AddForce(mover * forcaMovimento);
+		}
 	}
 
 	public void ativarControleMobile(){
